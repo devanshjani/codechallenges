b62cdf130209b72b1e886ccd68b5dd2a67146554
Devansh <devansh.jani@mysinglesource.io>
2019-04-09 14:29:30 +1200

    more changes to modelling

--- document_classification.py
+++ document_classification.py
@@ -4,20 +4,14 @@
 # Challenge Link
 # https://www.hackerrank.com/challenges/fraudulent-activity-notifications
 
-import numpy as np
 import nltk
 from typing import List
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn.linear_model import SGDClassifier
-from nltk.stem import PorterStemmer, WordNetLemmatizer
-from nltk.corpus import stopwords
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 __author__ = 'Devansh Jani <devanshjani@gmail.com>'
-
-stemmer = PorterStemmer()
-lammer = WordNetLemmatizer()
-words = stopwords.words("english")
 
 
 def read_training_data() -> (List, List):
@@ -38,38 +32,48 @@ def read_training_data() -> (List, List):
     return training_data, target
 
 
-def predict_document_class(test_data: np.array):
+def predict_document_class(validation_data: List[str]):
     """
     Predict document class from model trained.
 
-    :param test_data: Model validation using test data.
+    :param validation_data: Model validation using test data.
     """
     nltk.download('stopwords')
-    training_data, target = read_training_data()
-    training_data = [stemmer.stem(word) for word in training_data]
-    training_data = [lammer.lemmatize(word) for word in training_data]
-    target = list(map(int, target))
-    pipeline = Pipeline([
-        ('vec_idf', TfidfVectorizer(
-            analyzer='char_wb',
-            ngram_range=(1, 3),
-            stop_words=words)),
-        ('classifier', SGDClassifier(
-            loss='hinge',
-            penalty='l2',
-            alpha=1e-3,
-            n_iter=8,
-            random_state=42)),
-    ])
-    pipeline.fit(training_data, target)
 
-    for a_test in test_data:
-        print(*pipeline.predict(a_test), sep='\n')
+    data, target = read_training_data()
+    target = list(map(int, target))
+
+    (
+        training_data,
+        testing_data,
+        training_target,
+        testing_target) = train_test_split(
+        data, target, test_size=0.33)
+
+    tf_vec = TfidfVectorizer(
+        sublinear_tf=True,
+        norm='l2',
+        encoding='latin-1',
+        ngram_range=(1, 2),
+        stop_words='english',
+        max_features=10000
+    ).fit(training_data)
+
+    training_vec_data = tf_vec.transform(training_data)
+
+    model = LinearSVC()
+    model.fit(training_vec_data, training_target)
+
+    predictions = model.predict(tf_vec.transform(testing_data))
+    confusion_matrix(testing_target, predictions)  # Print this if needed
+    validation_vec_data = tf_vec.transform(validation_data)
+
+    for a_test in validation_vec_data:
+        print(*model.predict(a_test), sep='\n')
 
 
 if __name__ == '__main__':
     """Predict document class."""
     no_of_test_data = int(input())
-    testing_data = np.array(
-        [input().split(sep=',') for _ in range(no_of_test_data)], str)
+    testing_data = [input() for _ in range(no_of_test_data)]
     predict_document_class(testing_data)
